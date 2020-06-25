19b94931d92f90f33612b940af8ffd751fa85563
eysteinmeese91 <eystein.nordby.meese@pwc.com>
2020-06-25 16:19:57 +0200

    Oppdatert dokumentasjon for funksjonen capper.

--- R/capper.R
+++ R/capper.R
@@ -2,10 +2,9 @@
 #'
 #' @description a function that cuts a number between a floor and a ceiling.
 #'
-#' @param x
-#'
-#' @param floor
-#' @param ceiling
+#' @param x number or vector to cap
+#' @param floor floor of cap
+#' @param ceiling ceiling of cap
 #'
 #' @return a number either equal to x, floor or ceiling, depending on if x is
 #' smaller than floor or larger than ceiling, or between floor and ceiling.
