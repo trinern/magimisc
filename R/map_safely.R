e8cb2f731ecd735b107da56cbcefbd8431ddc974
markusmortensen <markus.mortensen@pwc.com>
2020-09-29 15:49:20 +0200

    Forbedret dokumentasjon

--- R/map_safely.R
+++ R/map_safely.R
@@ -4,29 +4,32 @@
 #' Apply a function to each element of a vector safely
 #'
 #' Denne funksjonen er en wrapper rundt funksjonene map_dfr og safely fra purrr.
+#'
 #' Se hjelpeside for purrr::map for doks.
 #'
-#' @param .x A list or atomic vector
-#' @param .f A function, formula, or vector (not necessarily atomic)
-#' @param ... .
-#' @param .id .
+#' @inheritParams purrr::map_dfr
 #'
-#' @return
+#' @return Funksjonen returnerer et listeobjekt med to elementer.
+#' $result inneholder en data.frame med resultat for de elementene som kjørte suksessfullt,
+#' mens $error er en liste med samme lengde som .x. Elementene i listen er NULL for de elementene i .x
+#' som kjørte uten feil og feilobjektet for de som feilet.
+#'
+#'
 #' @export
 #'
 #' @examples
-#' a <- safely_map_dfr(c(-1, 1), ~ data.frame(a = rnorm(.)))
+#' safely_map_dfr(c(-1, 1), ~ data.frame(a = rnorm(.)))
 safely_map_dfr <- function(.x, .f, ..., .id = NULL) {
 
   # .f <- as_mapper(.f, ...) # Tror ikke denne trengs
 
   .f_safely <- purrr::safely(.f)
 
-  temp <- map(.x, .f_safely)
+  temp <- purrr::map(.x, .f_safely)
 
-  res_object <- map_dfr(temp, ~ as.data.frame(.$result))
+  res_object <- purrr::map_dfr(temp, ~ as.data.frame(.$result))
 
-  err_object <- map(temp, ~ .$error)
+  err_object <- purrr::map(temp, ~ .$error)
 
   list(result = res_object,
        error = err_object)
