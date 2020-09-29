f7c461da443794654653321ff186995d6cfe5f9e
markusmortensen <markus.mortensen@pwc.com>
2020-09-29 16:23:53 +0200

    lagt til eksempel

--- R/tabset_list.R
+++ R/tabset_list.R
@@ -1,22 +1,46 @@
 #' Create tabset from a list
 #'
-#' @param list A \code{list} of elements to be printed in each tab
-#' @param header_level An \code{integer} specifying the level of the headers
+#' Hvert element i listen blir printet i en "tab" i en markdown rapport.
 #'
+#'
+#'
+#' @param list A \code{list} of elements to be printed in each tab
+#' @param header_level An \code{integer} specifying the level of the headers. Må være en mer enn overskriften over.
+#' @param header_names Vektor med navn på tabs.
 #' @return
 #' @export
 #'
 #' @examples
+#' ```{r cars}
+#' my_plot_function <- function(cylinders) {
+#' mtcars %>%
+#' filter(cyl == cylinders) %>%
+#' ggplot(aes(hp, mpg)) +
+#' geom_point()
+#' }
+#' p <- purrr::map(c(4, 6, 8), my_plot_function)
+#' ```
 #'
+#' # Overskrift {.tabset}
+#' ```{r pressure, results="asis"}
+#' tabset_list(p, 2)
+#' ```
 #'
-tabset_list <- function(list, header_level) {
+tabset_list <- function(list, header_level, header_names = NULL) {
 
   warning("results must be set to 'asis' in chunk options")
 
-  names = names(list)
-
-  if (is.null(names)) {
-    stop("cannot create tabsets for a list without names")
+  names <- names(list)
+  if (is.null(header_names)) {
+    list_names <- names(list)
+    if (is.null(list_names)) {
+      warning("Mangler navn")
+      names <- letters[1:length(list)]
+    } else {
+      names <- list_names
+    }
+  } else {
+    names <- header_names
   }
 
   hash = rep("#", header_level)
